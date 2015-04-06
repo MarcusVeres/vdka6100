a42045d96bd32d944c6e5c210322b0d5c85f8bf6
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 22:42:52 -0400

    adds terms and conditions page :: updates styles

---
+++ terms.php
@@ -0,0 +1,30 @@
+<?php include('static/partials/header.php'); ?>
+
+<?php include('static/partials/main-navigation.php'); ?>
+
+<section class="bg-off-white">
+<div class="content-wrapper text">
+
+    <h1 class="big">
+        Terms and Conditions
+    </h1>
+
+    <p class="copy">
+        When using or entering this website, www.VDKA6100.com (the “Site”), operated by Artisan Spirit Merchants USA (“ASM”), you agree to and accept all of these Terms and Conditions including ASM’s Privacy Policy which is incorporated by reference. If you do not agree to or accept these Terms and Conditions or the Privacy Policy, please do not use the Site and leave the Site immediately.
+    </p>
+    <p class="copy">
+        ASM, at its sole discretion, and at any time, reserves the right to modify, amend, change, add, suspend, cancel, or remove any portions, features or content of the Site, or these Terms and Conditions. By using the Site, you agree to be bound by any such revisions.
+    </p>
+
+    <h3 class="copy">
+        1. Age Restriction
+    </h3>
+    <p class="copy">
+        If you are a resident of the United States of America, you must be 21 years of age or older to use the Site , and if you are not at least 21 years of age, you must leave the Site immediately. If you reside in a country other than the United States of America, you must be of legal age to purchase alcoholic beverages in your country of residence to use the Site. If you are not of legal age to purchase alcoholic beverages. 
+    </p>
+
+</div>
+</section>
+
+<?php include('static/partials/footer.php'); ?>
+
