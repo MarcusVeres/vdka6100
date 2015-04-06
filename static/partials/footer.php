a7100ad00141d78e40bab3f2d3b1c6294d453457
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 21:54:30 -0400

    styles the fuck out of the footer sitemaps

--- static/partials/footer.php
+++ static/partials/footer.php
@@ -1,6 +1,68 @@
 
 <section class="footer">
-    <div class="content-wrapper">
+    <div class="content-wrapper flush">
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+
+                <ul class="sitemap disclaimers">
+                    <li>
+                        Please drink responsibly: VDKA6100 &tm; Vodka.
+                    </li>
+                    <li>
+                        Product of New Zealand. Distill. 40% ALC/VOL
+                    </li>
+                    <li>
+                        Imported by VDKA6100, MANHASSET, NY
+                    </li>
+                </ul>
+
+            </div>
+            <div class="col-xs-12 col-sm-3">
+
+                <ul class="sitemap legal">
+                    <li>
+                        <a href="/privacy">
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/terms">
+                            Terms and Conditions
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/contact">
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="col-xs-12 col-sm-3">
+
+                <ul class="sitemap social">
+                    <li>
+                        <a href="http://twitter.com" target="_blank">
+                            <i class="fa fa-fw fa-instagram"></i> 
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://twitter.com" target="_blank">
+                            <i class="fa fa-fw fa-facebook"></i> 
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://twitter.com" target="_blank">
+                            <i class="fa fa-fw fa-twitter"></i> 
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+
+        <!--
         <ul class="sitemap">
             <li><a href="/#staffing">Staffing</a></li> 
             <li><a href="/special-events">Special Events</a></li> 
@@ -12,6 +74,8 @@
         <p class="copyright">
             &copy; 2015 EventServicesToronto
         </p>
+        -->
+
     </div>
 </section>
 
