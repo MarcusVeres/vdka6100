fd1f9f1810713598b21f5a759e7bd3e1883f23de
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 21:54:13 -0400

    unlinks icomoon styles

--- static/partials/header.php
+++ static/partials/header.php
@@ -19,7 +19,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
+    <!--
     <link rel="stylesheet" href="/assets/icomoon.css"/>
+    -->
     <link rel="stylesheet" href="/assets/css/styles.min.css"/>
 
     <!-- custom -->
