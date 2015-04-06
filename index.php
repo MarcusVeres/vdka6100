3c7d8f50c13b7ce22fbe051ec5e6246f607cf3cb
Marcus Veres <marcus.veres@gmail.com>
2015-04-06 00:37:34 -0400

    progress made on landing page

--- index.php
+++ index.php
@@ -2,38 +2,52 @@
 
 <section class="uber">
 
-    <div class="display-table">
-        <div class="display-row">
-            <div class="display-cell">
-                this is content
-            </div>
-        </div>
-    </div>
+    <div class="display-row">
+        <div class="display-cell">
 
-</section>
-
-<!--
-<section class="banner main">
-
-    <div class="banner-main-inner-wrapper">
-    <div class="banner-main-inner-content">
-
-        <div class="content-wrapper unlimited">
-
-            <h1>
-                Welcome 
-            </h1>
-            <h2 class="tagline">
-                Friend
+            <h2 class="tagline wide">
+                Confirm your age to enter
             </h2>
 
-        </div>
+            <div class="inline">
+                <button class="btn line">
+                    Over 21
+                </button>
+                <button class="btn line">
+                    Under 21
+                </button>
+            </div>
 
+            <div class="checkbox">
+                <label><input type="checkbox" value="">Remember Me</label>
+            </div>
+
+        </div>
+        <div class="display-cell" style="width: 40%">
+            <img src="/assets/img/bottle.png" />
+        </div>
     </div>
+
+    <div class="display-row" style="height: 100px; width: 100%; border-top: 1px solid gray;">
+        <div class="display-cell agreement-wrapper">
+
+            <div class="implicit-agreement">
+                <p class="fine-print">
+                    By entering this site you agree to our
+                    <a href="/terms">
+                        Terms and Conditions
+                    </a>
+                    and
+                    <a href="/privacy">
+                        Privacy Policy
+                    </a>
+                </p>
+            </div>
+
+        </div>
     </div>
 
 </section>
--->
 
 </body>
 </html>
