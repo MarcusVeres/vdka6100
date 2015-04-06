21c9d6db706f8bc1d2b5f318ba511b7ad85739ec
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 22:47:19 -0400

    renames rdn page to vision :: adds privacy page

---
+++ privacy.php
@@ -0,0 +1,27 @@
+<?php include('static/partials/header.php'); ?>
+
+<?php include('static/partials/main-navigation.php'); ?>
+
+<section class="bg-off-white">
+<div class="content-wrapper text">
+
+    <h1 class="big">
+        Privacy Policy
+    </h1>
+
+    <p class="copy">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis nulla iaculis arcu tincidunt, ac bibendum velit porttitor. Pellentesque tincidunt id enim non venenatis. Aliquam et varius eros. Sed dictum est sit amet elementum iaculis. Morbi nec feugiat turpis. Praesent pretium hendrerit ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+    </p>
+
+    <h3 class="copy">
+        Ut lobortis nisl ipsum
+    </h3>
+    <p class="copy">
+        Maecenas in purus eget eros accumsan varius ultrices at orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam commodo leo ut ipsum convallis bibendum. Suspendisse elementum dolor a dictum tincidunt. Proin pulvinar elit ut interdum accumsan. Quisque dapibus nisi nec sem lacinia scelerisque. Curabitur scelerisque rhoncus felis non sodales. Donec velit diam, malesuada eget dictum tincidunt, facilisis et sapien. Ut auctor, metus in laoreet vestibulum, ex nunc faucibus velit, vitae convallis mauris leo eu arcu. Donec scelerisque orci ut ligula venenatis convallis. Phasellus aliquam molestie eros quis ullamcorper. Aenean ultrices nisl lorem, et rutrum diam faucibus ut.
+    </p>
+
+</div>
+</section>
+
+<?php include('static/partials/footer.php'); ?>
+
---
+++ vision.php
@@ -0,0 +1,49 @@
+<?php include('static/partials/header.php'); ?>
+
+<?php include('static/partials/main-navigation.php'); ?>
+
+<!-- header -->
+<section class="banner main rdn">
+
+    <div class="banner-main-inner-wrapper">
+    <div class="banner-main-inner-content">
+
+        <div class="content-wrapper tight">
+
+            <h2 class="tagline">
+                The best place for us to launch VDKA6100 is Tribeca.
+            </h2>
+
+        </div>
+
+    </div>
+    </div>
+
+</section>
+<!-- / header banner -->
+
+<section class="bg-black">
+<div class="content-wrapper text bg-off-white">
+
+    <div class="inkwell">
+        <h2>
+            &quot;Almost two years ago, James Packer asked me if I would like to help him develop and market a new vodka brand.&quot;
+        </h2>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <p class="copy">
+                One of the companies that he works with, Artisan Spirit Merchants, had uncovered a source of extraordinary vodka in New Zealand, which seems a world away to Americans but to the Australians, New Zealand is right next door. I tried the product and agreed with them, it’s great vodka. I showed the vodka to the team at Nobu and at The Greenwich Hotel and they also agreed that there was something special about it. So we helped hone the vodka and started to co-create the brand. 
+            </p>
+            <p class="copy">
+                We soft-launched VDKA 6100 in April last year at the Tribeca Film festival and over the past 12 months we have been refining the vodka, the proposition and the packaging. We have garnered opinion from super-premium vodka drinkers in the United States and now we’re ready to launch the latest version of VDKA 6100 across the globe, starting here in Tribeca. 
+            </p>
+        </div>
+    </div>
+
+</div>
+</section>
+
+<?php include('static/partials/footer.php'); ?>
+
