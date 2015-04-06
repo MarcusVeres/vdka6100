0f61b8f7fceeb84d3bf12c44190e4e06cf35f4a5
Marcus Veres <marcus.veres@gmail.com>
2015-04-06 16:39:58 -0400

    fixes positioning for firefox

--- home.php
+++ home.php
@@ -5,29 +5,30 @@
 <!-- header -->
 <section class="banner main full home">
 
-<div class="display-table" style="height: 100%; position: absolute; padding: 0 12%;">
-    <div class="display-row" style="height: 100%;">
+    <div class="display-table" style="height: 100%; padding: 0 12%;">
+        <div class="display-row" style="height: 100%;">
 
-        <div class="display-cell">
-            <h2 class="tagline">
-                As unique as the <br class="hide-from-mobile"> Tribeca Film Festival
-            </h2>
-            <a class="learn-more" href="/vdka6100">
-                Find Out More &raquo;
-            </a>
-        </div>
-        <!--
-        <div class="display-cell" style="width: 40%">
-            <img src="/assets/img/bottle.png" />
-        </div>
-        -->
- 
-        <div class="center-float">
-            <img src="/assets/img/bottle.png" />
-        </div>
+            <div class="display-cell">
+                <h2 class="tagline">
+                    As unique as the <br class="hide-from-mobile"> Tribeca Film Festival
+                </h2>
+                <a class="learn-more" href="/vdka6100">
+                    Find Out More &raquo;
+                </a>
+            </div>
 
+            <!--
+            <div class="display-cell" style="width: 40%">
+                <img src="/assets/img/bottle.png" />
+            </div>
+            -->
+     
+        </div>
     </div>
-</div>
+
+    <div class="center-float">
+        <img src="/assets/img/bottle.png" />
+    </div>
 
 </section>
 <!-- / header banner -->
