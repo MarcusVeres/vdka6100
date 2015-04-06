12df4d23753e3a6f2ce8eaf7e98e9142514538b9
Marcus Veres <marcus.veres@gmail.com>
2015-04-06 00:55:06 -0400

    getting tired :: last attempt at landing page

--- index.php
+++ index.php
@@ -2,48 +2,55 @@
 
 <section class="uber">
 
-    <div class="display-row">
-        <div class="display-cell">
+    <div class="display-table" style="height: 90%">
 
-            <h2 class="tagline wide">
-                Confirm your age to enter
-            </h2>
+        <img class="logo" src="/assets/img/logo.png" />
 
-            <div class="inline">
-                <button class="btn line">
-                    Over 21
-                </button>
-                <button class="btn line">
-                    Under 21
-                </button>
+        <div class="display-row">
+            <div class="display-cell">
+
+                <h2 class="tagline wide">
+                    Confirm your age to enter
+                </h2>
+
+                <div class="inline">
+                    <button class="btn line">
+                        Over 21
+                    </button>
+                    <button class="btn line">
+                        Under 21
+                    </button>
+                </div>
+
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Remember Me</label>
+                </div>
+
             </div>
-
-            <div class="checkbox">
-                <label><input type="checkbox" value="">Remember Me</label>
+            <div class="display-cell" style="width: 40%">
+                <img src="/assets/img/bottle.png" />
             </div>
-
-        </div>
-        <div class="display-cell" style="width: 40%">
-            <img src="/assets/img/bottle.png" />
         </div>
     </div>
 
-    <div class="display-row" style="height: 100px; width: 100%; border-top: 1px solid gray;">
-        <div class="display-cell agreement-wrapper">
+    <div class="display-table" style="height: 8%">
+        <div class="display-row" style="height: 100px; width: auto; border-top: 1px solid gray;">
+            <div class="display-cell agreement-wrapper">
 
-            <div class="implicit-agreement">
-                <p class="fine-print">
-                    By entering this site you agree to our
-                    <a href="/terms">
-                        Terms and Conditions
-                    </a>
-                    and
-                    <a href="/privacy">
-                        Privacy Policy
-                    </a>
-                </p>
+                <div class="implicit-agreement">
+                    <p class="fine-print">
+                        By entering this site you agree to our
+                        <a href="/terms">
+                            Terms and Conditions
+                        </a>
+                        and
+                        <a href="/privacy">
+                            Privacy Policy
+                        </a>
+                    </p>
+                </div>
+
             </div>
-
         </div>
     </div>
 
