35ea58c61ef37d3d959834d6a1efd63b616ef4fb
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 21:22:54 -0400

    adds rdn page and related styles

---
+++ rdn.php
@@ -0,0 +1,77 @@
+<section>
+
+<?php include('static/partials/header.php'); ?>
+
+<!-- the nav -->
+<section>
+
+    <div class="logo">
+        VDKA 6100
+    </div>
+
+    <ul class="main-nav">
+        <li>
+            <a href="/vkda6100">
+                VDKA 6100
+            </a>
+        </li>
+        <li>
+            <a href="/vision">
+                The Vision
+            </a>
+        </li>
+        <li>
+            <a href="/tff">
+                TFF
+            </a>
+        </li>
+        <li>
+            <a href="/contact">
+                Contact
+            </a>
+        </li>
+    </ul>
+
+</section>
+
+<!-- header -->
+<section class="banner main rdn">
+
+    <div class="banner-main-inner-wrapper">
+    <div class="banner-main-inner-content">
+
+        <div class="content-wrapper unlimited">
+
+            <h2 class="tagline">
+                The best place for us to launch VDKA6100 is Tribeca.
+            </h2>
+
+        </div>
+
+    </div>
+    </div>
+
+</section>
+<!-- / header banner -->
+
+<section class="bg-black">
+<div class="content-wrapper text bg-off-white">
+
+    <div class="inkwell">
+        <h2 class="tagline">
+            &quot;Almost two years ago, James Packer asked me if I would like to help him develop and market a new vodka brand.&quot;
+        </h2>
+    </div>
+
+    <p class="copy">
+        One of the companies that he works with, Artisan Spirit Merchants, had uncovered a source of extraordinary vodka in New Zealand, which seems a world away to Americans but to the Australians, New Zealand is right next door. I tried the product and agreed with them, it’s great vodka. I showed the vodka to the team at Nobu and at The Greenwich Hotel and they also agreed that there was something special about it. So we helped hone the vodka and started to co-create the brand. 
+    </p>
+    <p class="copy">
+        We soft-launched VDKA 6100 in April last year at the Tribeca Film festival and over the past 12 months we have been refining the vodka, the proposition and the packaging. We have garnered opinion from super-premium vodka drinkers in the United States and now we’re ready to launch the latest version of VDKA 6100 across the globe, starting here in Tribeca. 
+    </p>
+
+</div>
+</section>
+
+<?php include('static/partials/footer.php'); ?>
+
