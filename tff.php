af901047a862e36bd2e06832189ce24f33d4bf79
Marcus Veres <marcus.veres@gmail.com>
2015-04-05 22:59:44 -0400

    adds tff page :: adds assets for tff page

---
+++ tff.php
@@ -0,0 +1,56 @@
+<?php include('static/partials/header.php'); ?>
+
+<?php include('static/partials/main-navigation.php'); ?>
+
+
+<section class="banner main tff">
+
+    <div class="banner-main-inner-wrapper">
+    <div class="banner-main-inner-content">
+
+        <div class="content-wrapper tight">
+
+            <img class="centerpiece" src="/assets/img/tff-logo.png" title="Tribeca Film Festival" alt="TFF Logo" />
+
+        </div>
+
+    </div>
+    </div>
+
+</section>
+
+
+<section class="bg-black">
+<div class="content-wrapper text bg-off-white">
+
+    <div class="inkwell teal">
+        <h2>
+            Find the perfect spot to enjoy VDKA 6100
+        </h2>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            Placeholder for locations.
+        </div>
+    </div>
+
+    <div class="inkwell teal">
+        <h2>
+            Enter our sweepstakes!
+        </h2>
+    </div>
+
+    <img class="poster" src="/assets/img/tff-sweepstakes.jpg" alt="TFF Sweepstakes" />
+
+    <div class="inkwell teal">
+        <h2>
+            See what our fans are saying!
+        </h2>
+    </div>
+
+</div>
+</section>
+
+<?php include('static/partials/footer.php'); ?>
+
