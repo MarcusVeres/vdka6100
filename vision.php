7b238cafa6de3a1813132b8bc726392adac4ed13
Marcus Veres <marcus.veres@gmail.com>
2015-04-07 16:16:10 -0400

    adds different vision image :: updates copy and styles on vision page

--- vision.php
+++ vision.php
@@ -3,16 +3,17 @@
 <?php include('static/partials/main-navigation.php'); ?>
 
 <!-- header -->
-<section class="banner main rdn">
+<section class="banner main vision">
 
     <div class="banner-main-inner-wrapper">
     <div class="banner-main-inner-content">
 
         <div class="content-wrapper tight">
 
-            <h2 class="tagline">
-                The best place for us to launch VDKA6100 is Tribeca.
+            <h2 class="tagline right">
+                Our Mission is to Create the World’s Most Luxurious Vodka.
             </h2>
+            <div class="clearfix"></div>
 
         </div>
 
@@ -23,21 +24,30 @@
 <!-- / header banner -->
 
 <section class="bg-black">
-<div class="content-wrapper text bg-off-white">
-
-    <div class="inkwell">
-        <h2>
-            &quot;Almost two years ago, James Packer asked me if I would like to help him develop and market a new vodka brand.&quot;
-        </h2>
-    </div>
+<div class="content-wrapper pad-top text bg-off-white">
 
     <div class="row">
         <div class="col-xs-12">
             <p class="copy">
-                One of the companies that he works with, Artisan Spirit Merchants, had uncovered a source of extraordinary vodka in New Zealand, which seems a world away to Americans but to the Australians, New Zealand is right next door. I tried the product and agreed with them, it’s great vodka. I showed the vodka to the team at Nobu and at The Greenwich Hotel and they also agreed that there was something special about it. So we helped hone the vodka and started to co-create the brand. 
+                What is luxury? 
             </p>
             <p class="copy">
-                We soft-launched VDKA 6100 in April last year at the Tribeca Film festival and over the past 12 months we have been refining the vodka, the proposition and the packaging. We have garnered opinion from super-premium vodka drinkers in the United States and now we’re ready to launch the latest version of VDKA 6100 across the globe, starting here in Tribeca. 
+                To us, it’s about making smarter choices. It’s about having a quiet confidence that allows you to focus as much on what you choose to leave out, as what you choose to put in. 
+            </p>
+            <p class="copy">
+                It’s choosing to craft our vodka with whey, rather than the traditional grain or potatoes, because whey delivers a silkier, smoother taste that is more pleasurable to the mouth, with less of the burn found in other spirits.  
+            </p>
+            <p class="copy">
+                It’s choosing to distill our vodka 3 times, and filter it through a bed of carbon and pure New Zealand spring water; a process that results in less toxic methanol than any other vodka. 
+            </p>
+            <p class="copy">
+                It’s choosing to leave out any extraneous elements and focus only on the best. So that’s what’s left is a smooth luxury spirit with light lemon and vanilla hints, delivering a sublime drinking experience. 
+            </p>
+            <p class="copy">
+                Most importantly, to our drinkers, luxury isn’t hype, or photo shoots on yachts. It’s a real, discernible difference that starts when you pick up our understatedly stylish bottle to the point when the spirit hits the tongue. 
+            </p>
+            <p class="copy">
+                One of the first people to appreciate the unique taste and quality that goes into VDKA 6100 was Robert De Niro, American actor and business entrepreneur. As the co-creator of VDKA 6100 he is involved in launching the brand in the US and eventually worldwide. A global tastemaker, Mr. De Niro was tapped from the beginning for his reputation as a visionary in the hospitality business and for his renowned ability for making good choices.
             </p>
         </div>
     </div>
