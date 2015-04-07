cf01d0a575b723ccb178e3cdfdd171eae0ab0f01
Marcus Veres <marcus.veres@gmail.com>
2015-04-07 16:36:16 -0400

    restructures and styles privacy page

--- privacy.php
+++ privacy.php
@@ -10,14 +10,80 @@
     </h1>
 
     <p class="copy">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis nulla iaculis arcu tincidunt, ac bibendum velit porttitor. Pellentesque tincidunt id enim non venenatis. Aliquam et varius eros. Sed dictum est sit amet elementum iaculis. Morbi nec feugiat turpis. Praesent pretium hendrerit ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        When using or entering this website, www.VDKA6100.com (the “Site”), operated by Artisan Spirit Merchants USA (“ASM”), you agree to and accept ASM’s Privacy Policy which is set forth below. If you do not agree to or accept this Privacy Policy in its entirety, please do not use the Site and leave the Site immediately.
+    </p>
+    <p class="copy">
+        ASM, at its sole discretion, and at any time, reserves the right to modify, amend, change, aspects of the Privacy Policy at any time. By using the Site, you agree to be bound by any such revisions.
     </p>
 
-    <h3 class="copy">
-        Ut lobortis nisl ipsum
-    </h3>
+    <ol>
+        <li>
+            <h3 class="copy"> 
+                AGE RESTRICTION
+            </h3>
+            <p class="copy">
+                If you are a resident of the United States of America, you must be 21 years of age or older to use the Site. If you reside in a country other than the United States of America, you must be of legal age to purchase alcoholic beverages in your country of residence to use the Site. If you are not of legal age to purchase alcoholic beverages in your country of residence, do not provide us with your personal information, and leave the Site immediately. If you are the parent or legal guardian of someone under the legal age to purchase alcoholic beverages who has provided ASM with personal information by violating the Terms and Conditions of the Site, please contact ASM immediately, and we will remove the personal information from our records.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                COLLECTION OF PERSONAL INFORMATION
+            </h3>
+            <p class="copy">
+                ASM may collect personal information that you choose to provide us such as your e-mail address, name, mailing address or phone number. We may also collect anonymous demographic information which is not unique to you such as your ZIP code, age, gender, preferences and interests. If you do not want any of the foregoing information collected by ASM, do not submit it.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                OUR USE OF PERSONAL INFORMATION
+            </h3>
+            <p class="copy">
+                ASM uses the personal information that you have voluntarily submitted to inform you of products, services or promotions that may be of interest to you or to provide you with information that you request. ASM may also contact you to participate in surveys to conduct marketing research. ASM does not sell personal information to third-parties, but may release personal information when required to do so by law, or when in ASM’s sole discretion, it believes it is necessary to (i) comply with legal process served upon ASM, (ii) protect and defend the rights or property of ASM, or (iii) protect and defend the rights, property or safety of users of the Site or the public.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                COOKIES
+            </h3>
+            <p class="copy">
+                The Site uses “cookies” to help save you time when you visit and use the Site. Cookies allow the retrieval of information that you previously provided to be retrieved. You may modify your web browser to accept or reject cookies. However, rejecting cookies may impair your ability to fully use the Site.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                SECURITY OF YOUR PERSONAL INFORMATION
+            </h3>
+            <p class="copy">
+                ASM will safeguard the personal information that you submit, however, ASM does not, in any way, guarantee the complete security of any personal information it receives.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                OPT OUT POLICY
+            </h3>
+            <p class="copy">
+                You may opt-out of any use of your personal information by sending an e-mail to the following address: Requestusa@asmliquor.com. Please put “Opt-Out” in the subject line of all e-mail notifications.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy"> 
+                CONTACT US
+            </h3>
+            <p class="copy">
+                For more information governing your use of the Site, please review our <a href="/terms">Terms and Conditions</a>. 
+                If you have any questions or concerns regarding this Privacy Policy, the Site or the Terms and Conditions, please send an e-mail to the following address: 
+                <a href="mailto:requestusa@asmliquor.com">Requestusa@asmliquor.com</a>.
+            </p>
+        </li>
+    </ol>
+
+    <hr>
+
     <p class="copy">
-        Maecenas in purus eget eros accumsan varius ultrices at orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam commodo leo ut ipsum convallis bibendum. Suspendisse elementum dolor a dictum tincidunt. Proin pulvinar elit ut interdum accumsan. Quisque dapibus nisi nec sem lacinia scelerisque. Curabitur scelerisque rhoncus felis non sodales. Donec velit diam, malesuada eget dictum tincidunt, facilisis et sapien. Ut auctor, metus in laoreet vestibulum, ex nunc faucibus velit, vitae convallis mauris leo eu arcu. Donec scelerisque orci ut ligula venenatis convallis. Phasellus aliquam molestie eros quis ullamcorper. Aenean ultrices nisl lorem, et rutrum diam faucibus ut.
+        &copy; 2014 Artisan Spirits Merchants USA, New York, NY.
+    </p>
+    <p class="copy">
+        Updated January 10, 2014.
     </p>
 
 </div>
