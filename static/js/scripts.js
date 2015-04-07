6299a248e0052882f04707dc674f6f8e13170233
Marcus Veres <marcus.veres@gmail.com>
2015-04-06 21:56:24 -0400

    adds styles to map :: hides some map controls

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,8 @@
 (function(){
 
+    // color properties for styling the map
+    var map_styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+
     // stores location data
     var json_data = 'unset';
 
@@ -29,8 +32,11 @@
         // configure the map
         var mapOptions = {
             center: new google.maps.LatLng( 40.716269 , -74.008632 ),
-            zoom: 12,
-            // styles: style
+            zoom: 14,
+            styles: map_styles,
+            mapTypeControl: false,
+            panControl: false,
+            zoomControl: false
         };
 
         // draw the map on the map canvas dom element
