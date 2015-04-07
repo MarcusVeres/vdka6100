9c9691eaea452662a1be5360a24ed3e409e7d6c8
Marcus Veres <marcus.veres@gmail.com>
2015-04-07 16:30:54 -0400

    styles and formats the full terms page

--- terms.php
+++ terms.php
@@ -16,11 +16,123 @@
         ASM, at its sole discretion, and at any time, reserves the right to modify, amend, change, add, suspend, cancel, or remove any portions, features or content of the Site, or these Terms and Conditions. By using the Site, you agree to be bound by any such revisions.
     </p>
 
-    <h3 class="copy">
-        1. Age Restriction
-    </h3>
+    <ol class="legal">
+        <li>
+            <h3 class="copy">
+                AGE RESTRICTION
+            </h3>
+            <p class="copy">
+                If you are a resident of the United States of America, you must be 21 years of age or older to use the Site , and if you are not at least 21 years of age, you must leave the Site immediately. If you reside in a country other than the United States of America, you must be of legal age to purchase alcoholic beverages in your country of residence to use the Site. If you are not of legal age to purchase alcoholic beverages in your country of residence, you must leave the Site immediately.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                SITE MATERIAL
+            </h3>
+            <p class="copy">
+                ASM is the owner of the Site and all material and content on the Site (including, without limitation, text, images, audio, video and software.) You agree that you are permitted to use this material and/or content and the Site only as explicitly stated herein. You may access this Site for your individual personal use only and not for any commercial purpose. Material and/or content from this Site may not be used in any manner without the prior written consent of ASM.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                INTELLECTUAL PROPERTY RIGHTS
+            </h3>
+            <p class="copy">
+                All of the intellectual property appearing or contained on the Site is owned by ASM. Nothing in these Terms and Conditions, or your use of the Site should be understood in any way as granting or conferring any license, right, title or interest in ASM’s intellectual property. Any use or reproduction of ASM’s intellectual property is prohibited.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                DISCLAIMER OF WARRANTIES
+            </h3>
+            <p class="copy">
+                Any use of the Site is at your own risk. The Site (including, without limitation, materials, content and functions) is provided to you "as is" without further representation or warranty of any kind, either express or implied. To the fullest extent permissible by law with regard to the Site, ASM and its officers, directors, employees, agents and assigns explicitly disclaim and make no representations or warranties of any kind whatsoever: for the accuracy, merchantability, fitness for a particular purpose, title or non-infringement of any materials or content on the Site; or that the Site is free of viruses or bugs that may infect, harm or cause damage to your computer equipment, network connections or any other property when you access or use the Site in any way.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                LIMITATION OF LIABILITY
+            </h3>
+            <p class="copy">
+                ASM, and its officers, directors, employees, subcontractors, agents, successors, or assigns of any of them, shall not be liable for any direct, indirect, incidental, special, or consequential damages (including attorneys' fees and lost profits) to you connected with your use of the Site, including its content and regardless of any negligence or otherwise of any of them. ASM does not guarantee that the functions contained on the Site will be uninterrupted or error-free, that the Site or its server will be free of viruses or other harmful components, or that defects will be corrected even if ASM is aware of them.
+                You acknowledge and agree that ASM is not responsible or liable for any loss, injury, claim, liability, or damage of any kind resulting from, arising out of, or in any way related to: (1) any errors in or omissions from the Site, including but not limited to technical inaccuracies and typographical errors; (2) any third party websites or content therein directly or indirectly accessed through links in the Site; (3) any information which is posted by any third-party, including without limitation, information posted on any related blog; (4) the unavailability of the Site or any portion of the Site; (5) your use of the Site; or (6) your use of any equipment or software in connection with the Site.
+                You agree and acknowledge that your sole remedy is to discontinue using the Site. In no event shall the total liability to you for any and all damages and losses and causes of action in the aggregate (whether in contract, tort or otherwise) arising from the use of the Site exceed $75.00.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                LINKED SITES
+            </h3>
+            <p class="copy">
+                The Site, may, from time to time, be linked to other third-party websites (“Third-Party Sites”). ASM provides such links solely at its own discretion and for your convenience. The display of such links does not constitute an endorsement or approval of Third-Party Sites. You agree and acknowledge that (1) ASM has no responsibility for Third-Party Sites or any materials or content contained therein or any goods or services sold, offered or provided through Third-Party Sites and (2) ASM is not liable for any losses or damages of any kind that you may incur by linking to or using Third-Party Sites or for any transactions you may conduct on Third-Party Sites. Your use of Third-Party Sites is solely at your own risk.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                INDEMNIFICATION
+            </h3>
+            <p class="copy">
+                You agree to indemnify and hold ASM, its parents, subsidiaries, affiliates, directors, officers employees, representatives, agents and assigns, harmless from any loss, damage, claim or demand, including reasonable attorneys’ fees and costs, made by any third-party due to or arising out of (1) your use of the Site in any manner, (2) the violation of these Terms and Conditions or (3) your violation of any rights of another.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                NO UNLAWFUL OR PROHIBITED USE
+            </h3>
+            <p class="copy">
+                You agree and acknowledge that you will not use the Site for any unlawful purpose, for any purpose in violation of these Terms and Conditions or in any manner which may interfere, damage or disable the Site.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                PERSONAL INFORMATION
+            </h3>
+            <p class="copy">
+                By using the Site you may be asked to provide personal information to ASM. In that regard, you agree to provide truthful and accurate information. Please review our Privacy Policy with respect to how Artisan may use your personal information.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                DIGITAL MILLENNIUM COPYRIGHT ACT
+            </h3>
+            <p class="copy">
+                If you believe any materials or content on the Site infringe a copyright, you should notify ASM by sending a written notice with the following information to the address below:
+            </p>
+            <ul>
+                <li>A physical or electronic signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+                <li>Identification of the copyrighted work claimed to have been infringed, or, if multiple copyrighted works on the Site are covered by a single notification, a representative list of such works on.</li>
+                <li>Identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled, and information reasonably sufficient to permit ASM to locate the material. Providing URLs in the body of an email is the best way to help us locate content quickly.</li>
+                <li>Information reasonably sufficient to permit ASM to contact you, such as an address, telephone number, and, if available, an electronic mail address at which the complaining party may be contacted.</li>
+                <li>A statement that the complaining party has a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.</li>
+                <li>A statement that the information in the notification is accurate, and under penalty of perjury, that the complaining party is authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+            </ul>
+            <p class="copy">
+                All notices sent pursuant to the Digital Millennium Copyright Act should be sent to: Artisan Spirit Merchants USA, Attention: Copyright Agent, 250 Lafayette Street 4th floor, New York, NY 10012, or e-mailed to: Requestusa@asmliquor.com. Please put “Notice of Infringement” in the subject line of all e-mail notifications.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                ENTIRE AGREEMENT
+            </h3>
+            <p class="copy">
+                These Terms and Conditions, including the Privacy Policy, are the sole and entire agreement between you and ASM in connection with the Site and supersede any and all other representations, agreements or promises of any kind, whether written or oral, between you and ASM.
+            </p>
+        </li>
+        <li>
+            <h3 class="copy">
+                GOVERNING LAW
+            </h3>
+            <p class="copy">
+                These Terms and Conditions, including the Privacy Policy, your use of the Site or any matter related to the Site shall be construed in accordance with and governed by the laws of the State of New York, notwithstanding conflict of law rules or principles. Any litigation or legal proceeding shall be subject to the exclusive jurisdiction of the courts of the State of New York.
+            </p>
+        </li>
+    </ol>
+
     <p class="copy">
-        If you are a resident of the United States of America, you must be 21 years of age or older to use the Site , and if you are not at least 21 years of age, you must leave the Site immediately. If you reside in a country other than the United States of America, you must be of legal age to purchase alcoholic beverages in your country of residence to use the Site. If you are not of legal age to purchase alcoholic beverages. 
+        &copy; 2014 Artisan Spirits Merchants USA, New York, NY.
+    </p>
+    <p class="copy">
+        Updated January 10, 2014.
     </p>
 
 </div>
