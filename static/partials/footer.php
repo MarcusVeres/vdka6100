c1db7285fe8423d4a07ec9ac74975e244f7f11cf
Marcus Veres <marcus.veres@gmail.com>
2015-04-08 14:52:24 -0400

    updates footer styles

--- static/partials/footer.php
+++ static/partials/footer.php
@@ -1,66 +1,56 @@
 
 <section class="footer">
-    <div class="content-wrapper flush wide">
+    <div class="content-wrapper flush">
 
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
+        <ul class="sitemap disclaimers">
+            <li>
+                Please drink responsibly: VDKA6100 &trade; Vodka.
+            </li>
+            <li>
+                Product of New Zealand. Distill. 40% ALC/VOL
+            </li>
+            <li>
+                Imported by VDKA6100, MANHASSET, NY
+            </li>
+        </ul>
 
-                <ul class="sitemap disclaimers">
-                    <li>
-                        Please drink responsibly: VDKA6100 &tm; Vodka.
-                    </li>
-                    <li>
-                        Product of New Zealand. Distill. 40% ALC/VOL
-                    </li>
-                    <li>
-                        Imported by VDKA6100, MANHASSET, NY
-                    </li>
-                </ul>
+        <ul class="sitemap legal">
+            <li>
+                <a href="/privacy">
+                    Privacy Policy
+                </a>
+            </li>
+            <li>
+                <a href="/terms">
+                    Terms and Conditions
+                </a>
+            </li>
+            <li>
+                <a href="#" data-toggle="modal" data-target="#basicModal">
+                    Contact Us
+                </a>
+            </li>
+        </ul>
 
-            </div>
-            <div class="col-xs-12 col-sm-3">
-
-                <ul class="sitemap legal">
-                    <li>
-                        <a href="/privacy">
-                            Privacy Policy
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/terms">
-                            Terms and Conditions
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" data-toggle="modal" data-target="#basicModal">
-                            Contact Us
-                        </a>
-                    </li>
-                </ul>
-
-            </div>
-            <div class="col-xs-12 col-sm-3">
-
-                <ul class="sitemap social">
-                    <li>
-                        <a href="http://instagram.com/vdka6100" target="_blank">
-                            <i class="fa fa-fw fa-instagram"></i> 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://facebook.com/VDKA6100" target="_blank">
-                            <i class="fa fa-fw fa-facebook"></i> 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://twitter.com/VDKA6100" target="_blank">
-                            <i class="fa fa-fw fa-twitter"></i> 
-                        </a>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
+        <ul class="sitemap social">
+            <li>
+                <a href="http://instagram.com/vdka6100" target="_blank">
+                    <i class="fa fa-fw fa-instagram"></i> 
+                </a>
+            </li>
+            <li>
+                <a href="http://facebook.com/VDKA6100" target="_blank">
+                    <i class="fa fa-fw fa-facebook"></i> 
+                </a>
+            </li>
+            <li>
+                <a href="http://twitter.com/VDKA6100" target="_blank">
+                    <i class="fa fa-fw fa-twitter"></i> 
+                </a>
+            </li>
+        </ul>
+        
+        <div class="clearfix"></div>
 
     </div>
 </section>
