79adfd1ddc6b3d6aeb272fe0aa3fb5ffaa2b5623
Marcus Veres <marcus.veres@gmail.com>
2015-04-08 17:25:21 -0400

    imports and styles twitter feed

--- tribeca-film-festival.php
+++ tribeca-film-festival.php
@@ -128,6 +128,21 @@
         </h2>
     </div>
 
+    <div class="social-feed-wrapper">
+        
+        <!-- facebook -->
+
+        <!-- twitter -->
+        <div class="social-feed">
+            <a class="twitter-timeline" href="https://twitter.com/VDKA6100" data-widget-id="585914868575920129">Tweets by @VDKA6100</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+
+        <!-- instagram -->
+
+
+    </div>
+
 </div>
 </section>
 
