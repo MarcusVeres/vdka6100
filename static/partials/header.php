54bd74e76c2cf30a7c669780051aa29943741c2f
Marcus Veres <marcus.veres@gmail.com>
2015-06-02 15:23:53 -0400

    moves scripts into footer :: adds generator script

--- static/partials/header.php
+++ static/partials/header.php
@@ -20,25 +20,6 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/styles.min.css"/>
 
-    <!-- custom -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAdVeDCpF1gDswiV6rRNJF7Ibqi6aJAtvs&sensor=false"></script>
-
-    <!-- scripts -->
-    <script src="static/js/scripts.js"></script>
-
-    <!-- analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-62346902-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-
 </head>
 <body>
 
